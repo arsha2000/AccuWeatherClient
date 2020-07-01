05a1efea3eb933c067e9c3bcbbd462a6e4ac88d8
Mohammad Hassas <mohammadhassas.ot@gmail.com>
2020-07-01 14:47:54 +0300

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "AccuWeatherClient",
+    platforms: [
+        .iOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -25,5 +28,6 @@ let package = Package(
         .testTarget(
             name: "AccuWeatherClientTests",
             dependencies: ["AccuWeatherClient"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
